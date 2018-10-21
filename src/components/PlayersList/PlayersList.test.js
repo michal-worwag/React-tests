d5bd72d8e00412ce5efc17d4ec09567ced602ff5
Michał Worwąg <michalwo@gmail.com>
2018-10-21 12:08:30 +0200

    Add PlayersList component and test, add styles

---
+++ src/components/PlayersList/PlayersList.test.js
@@ -0,0 +1,49 @@
+import PlayersList from './PlayersList';
+import Player from '../Player/Player';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+it('renders without crashing', () => {
+  shallow(<PlayersList players={[]} />);
+});
+
+it('renders correct number of players', () => {
+  const players = [
+    {
+        name: 'Kunegunda',
+        score: 5
+    },
+    {
+        name: 'Antoś',
+        score: 0
+    }
+]
+const playerComponent = shallow(<PlayersList players={players} />);
+console.log(playerComponent.debug());
+const expectedPlayersNumber = playerComponent.find(Player).length;
+
+expect(expectedPlayersNumber).toEqual(2);
+});
+
+it('check onScoreUpdate', () => {
+    const players = [
+        {
+            name: 'Kunegunda',
+            score: 5
+        },
+        {
+            name: 'Antoś',
+            score: 0
+        }
+    ]
+
+    const mockedOnScoreUpdate = jest.fn();
+
+    const playerComponent = shallow(<PlayersList players={players} onScoreUpdate={mockedOnScoreUpdate} />);
+
+    const firstPlayer = playerComponent.find(Player).first();
+    const onPlayerScoreChange = firstPlayer.prop('onPlayerScoreChange');
+
+    onPlayerScoreChange(20);
+    expect(mockedOnScoreUpdate).toBeCalledWith(0, 20);
+});
