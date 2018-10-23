7d301f711f92fc7cf4482f268fe347c1c7cf1b92
Michał Worwąg <michalwo@gmail.com>
2018-10-23 12:18:47 +0200

    Updates

---
+++ precache-manifest.af52560b0e646b6924162bb2e175bb67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fb8e39dbfb1e0d109a7",
+    "url": "/ScoreKeeperApp/static/js/runtime~main.0fb8e39d.js"
+  },
+  {
+    "revision": "6a2e6516fdcab1ecd5ba",
+    "url": "/ScoreKeeperApp/static/js/main.6a2e6516.chunk.js"
+  },
+  {
+    "revision": "d6b04199414bc0479bcf",
+    "url": "/ScoreKeeperApp/static/js/1.d6b04199.chunk.js"
+  },
+  {
+    "revision": "6a2e6516fdcab1ecd5ba",
+    "url": "/ScoreKeeperApp/static/css/main.08d664d2.chunk.css"
+  },
+  {
+    "revision": "83059c5b953ceaf6eff48caa881febef",
+    "url": "/ScoreKeeperApp/index.html"
+  }
+];
